f9e3490651d34bea5e921fecf0dec8650a85d1ae
Sujithreddy33 <sujreddy@osius.com>
2016-09-08 13:28:54 +0530

    added all the modules

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ config.mongo.port = 27017;//19816;//27017
 config.mongo.password = '7643356f67a4a151206bb30aa04d47b9';
 config.mongo.username = 'bookmarkuser';
 config.mongo.dsportmlab = 'ds019816.mlab.com';
-config.web.port = process.env.WEB_PORT || 3411;
+config.web.port = process.env.PORT || 3411;
 console.log(config);
 
 
