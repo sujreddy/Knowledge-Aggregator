5473fdc78570c7ca724b1d2d9991f9de369e9368
Sujithreddy33 <sujreddy@osius.com>
2016-09-08 13:30:59 +0530

    added all the modules

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ config.mongo = {};
 config.web = {};
 
 config.mongo.host = 'localhost';
-config.mongo.port = 27017;//19816;//27017
+config.mongo.port = 19816;//19816;//27017
 config.mongo.password = '7643356f67a4a151206bb30aa04d47b9';
 config.mongo.username = 'bookmarkuser';
 config.mongo.dsportmlab = 'ds019816.mlab.com';
